98e1f2f96fc4c3d2605f81fd9622f0027e90c5a2
Druag <Shak.espeare@yandex.ru>
2026-06-09 00:09:19 +0800

    fixik

--- TextBattleWithMob/Program.cs
+++ TextBattleWithMob/Program.cs
@@ -133,7 +133,7 @@ namespace TextBattleWithMob
                                 string playerChoice = Console.ReadLine();
                                 if (!int.TryParse(playerChoice, out int playerChoiceInt))
                                 {
-                                    Console.WriteLine("Введите число от 1 до 2");
+                                    Console.WriteLine("Введите число от 1 до 3");
                                     continue;
                                 }
 
